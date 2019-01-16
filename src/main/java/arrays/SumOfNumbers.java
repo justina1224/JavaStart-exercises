4fc34d1639ca340ae9edf78c697cb993bde990a5
justina1224 <justyna.pilzno@gmail.com>
2019-01-16 01:36:33 +0100

    "arrays"

---
+++ src/main/java/arrays/SumOfNumbers.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class SumOfNumbers {
+    private static int[] array1 = {1,1,1};
+    private static int[] array2 = {2,2,2};
+
+    public static void main(String[] args) {
+        System.out.println("Suma wartosci z tablic: " + (array1[0] + array1[1] +
+                array1[2] + array2[0] + array2[1] + array2[2]));
+    }
+}
